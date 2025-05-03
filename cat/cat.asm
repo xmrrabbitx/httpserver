5536aa6caa3b05079bceda9bdf2540b9b1e80bd5
xmrrabbitx <hadi.mirzaie300@gmail.com>
2025-05-04 00:29:46 +0330

    finished params

--- cat/cat.asm
+++ cat/cat.asm
@@ -9,7 +9,6 @@ fdCat dq 0
 bufferCat rb 1024
 bytesReadCat dq 0
 
-
 macro open filenamecat
 
 	mov rax, SYS_OPEN
@@ -59,10 +58,33 @@ macro newline
 
 end macro
 
+macro paramsCheck
+
+	pop rcx ; rcx = 2, rsp += 8	
+	pop rsi ; rdi = argv[0], rsp += 8 (discard program name)
+	pop rsi ; rdi = argv[1], rsp += 8 (now points to NULL)
+
+	cmp rsi, 0 ;; check on NULL value
+	je emptyPrmErr
+
+	;;mov al, [rsi]    ; al = '-' first chars param
+	;;mov bl, [rsi+1]  ; bl = 'f' second cars param
+
+	;;cmp al, '-' ;; check on special chars
+	;;jne prmErr
+
+	mov rdi, filenameCat
+	mov rcx, 256
+	rep movsb ;; Copy until null terminator or RCX=0
+
+end macro
+
+
 segment readable executable
 entry cat
 cat:
-
+	paramsCheck
+	
 	open filenameCat
 
 	;; error handling
@@ -84,14 +106,40 @@ printErr:
 	mov rax, SYS_WRITE
 	mov rdi, 1
 	mov rsi, errorMsg_filenotfound
-	mov rdx, 1024
+	mov rdx, 26 ;; err message length
 	syscall
 
 	newline
 
 	jmp exit	
 
+
+prmErr:
+
+	mov rax, SYS_WRITE
+	mov rdi, 1
+	mov rsi, errorMsg_param
+	mov rdx, 17 ;; err message length
+	syscall
+
+	newline
+
+	jmp exit	
+
+emptyPrmErr:
+
+	mov rax, SYS_WRITE
+	mov rdi, 1
+	mov rsi, errorMsg_emptyParam
+	mov rdx, 16 ;; err message length
+	syscall
+
+	newline
+
+	jmp exit	
 segment readable writeable
-filenameCat db "1.txt", 0
+filenameCat db 256 dup(?) 
 del db 0xa, 0
 errorMsg_filenotfound db "No such file or directory!", 0
+errorMsg_param db "wrong parameters!", 0
+errorMsg_emptyParam db "empty file name!", 0

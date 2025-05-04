8d66088ab6333ba202e5103878466ad61279a346
xmrrabbitx <hadi.mirzaie300@gmail.com>
2025-05-04 18:44:07 +0330

    finsihed httpserver

--- httpserver.asm
+++ httpserver.asm
@@ -1,18 +1,20 @@
 format ELF64 executable
 
-
 af_inet = 2
 domain = af_inet ;;af_inet = 2
 type = 1 
 protocol = 0 ;; default value is 0
 
+fd dq 0
+bufferHtml rb 1024
+bytesReadHtml dq 0
 
 segment readable executable
 
 entry main
 main:
 	;;creating socket
-;;rdi   rsi   rdx   r10	  r8   r9
+	;;rdi   rsi   rdx   r10	  r8   r9
 	mov rax, 41
 	mov rdi, domain
 	mov rsi, type
@@ -45,35 +47,47 @@ main:
 
 	mov r13, rax ;;result of acceppt, client socket fd	
 	
-	;;read file
+	;;read client socket request _ its curl request
 	mov rax, 0
 	mov rdi, r13 ;;read client socket
-	mov rsi, buffer
-	mov rdx, 256
+	mov rsi, bufferHtml
+	mov rdx, 1024
 	syscall	
 
-	;;open file
+	;;open file index.html
 	mov rax, 2
 	mov rdi, path
 	mov rsi, 0
 	syscall
 
-	mov r14, rax
-
+	mov [fd], rax
+	
   	;;read index.html file
         mov rax, 0
-        mov rdi, r14 ;;read index file
-        mov rsi, buffer2
-        mov rdx, 256
+        mov rdi, [fd] ;;read index file
+        mov rsi, bufferHtml
+        mov rdx, 1024
         syscall
 
-	mov r15, rax
-	
+	mov [bytesReadHtml], rax	
+
+	;; close file
+    	mov rax, 3
+    	mov rdi, [fd]
+    	syscall
+
+    	;; write HTTP headers
+    	mov rax, 1
+    	mov rdi, r13
+    	mov rsi, http_header
+    	mov rdx, http_header_len
+    	syscall
+
 	;;write r14 into r13
 	mov rax, 1
 	mov rdi, r13
-	mov rsi, buffer2
-	mov rdx, r15
+	mov rsi, bufferHtml
+	mov rdx, [bytesReadHtml]
 	syscall
 
 	;;close 
@@ -88,6 +102,7 @@ main:
 
 	;jmp accept_loop	
 	
+exit:	
 	mov rax, 60
 	mov rdx, rdx
 	syscall
@@ -99,11 +114,12 @@ dw af_inet
 dw 0x901f
 dd 0
 dq 0
-buffer:
-db 256 dup 0 ;;dup is loop in zero times
-buffer2:
-db 256 dup 0
+
 path:
 db 'index.html',0
 
+http_header db 'HTTP/1.1 200 OK',13,10
+             db 'Content-Type: text/html',13,10
+             db 'Connection: close',13,10,13,10
+http_header_len = $ - http_header
 

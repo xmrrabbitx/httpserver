fb893c9c322f28ffc9487f7be4b1e035283c4697
xmrrabbitx <hadi.mirzaie300@gmail.com>
2025-05-14 01:44:47 +0330

    fix rax issues

--- httpserver.asm
+++ httpserver.asm
@@ -289,7 +289,6 @@ php_fpm:
 	;;fcgiHeaders r15, fcgi_headers_begin, fcgi_headersbegin_length
 	fcgiBeginRequest r15, fcgi_begin_request, fcgi_begin_request_length 
 
-	;;fcgiHeaders r15, fcgi_headers_params, fcgi_headersparams_length
 	fcgiParamsRequest r15, fcgi_params, fcgi_params_length
 		
 	fcgiEndParamsRequest r15, fcgi_end_params, fcgi_end_params_length
@@ -443,64 +442,46 @@ fcgi_begin_request:
 fcgi_begin_request_length = $ - fcgi_begin_request
 
 fcgi_params:
+    ; Header (8 bytes)
+    db 1, 4, 0, 1        ; version=1, type=4(PARAMS), requestId=1
+    db 0, 224, 0, 0      ; contentLength=224, padding=0
 
-	
-        db 1 ;; version = 1
-        db 4 ;; type = 4
-        db 0
-        db 1
-	db 0
-	db 48
-        db 0
-        db 0
+    ; Parameter 1: SCRIPT_FILENAME
+    db 0x0F, 0x00        ; nameLength=15
+    db 0x1F, 0x00        ; valueLength=31
+    db 'SCRIPT_FILENAME', 0
+    db '/home/ahmad/assembly/index.php', 0
 
-	;; struct in C: name_len value_len name_byte value_byte	
-	db 15 ;; name length
-	db 31 ;;  value length
-	db 'SCRIPT_FILENAME' ;; name byte
-	db "/home/ahmad/assembly/index.php" ;; value byte
-	
-	db 14
-	db 3
-	db "REQUEST_METHOD"
-	db "GET"
+    ; Parameter 2: REQUEST_METHOD
+    db 0x0E, 0x00        ; nameLength=14
+    db 0x03, 0x00        ; valueLength=3
+    db 'REQUEST_METHOD', 0
+    db 'GET', 0
 
-	db 14
-	db 1
-	db "CONTENT_LENGTH"
-	db "0"
-		
-	db 15
-	db 8
-	db "SERVER_PROTOCOL"
-	db "HTTP/1.1"
+    ; Parameter 3: CONTENT_LENGTH
+    db 0x0E, 0x00
+    db 0x01, 0x00
+    db 'CONTENT_LENGTH', 0
+    db '0', 0
 
-	db 17
-	db 7
-	db "GATEWAY_INTERFACE"
-	db "CGI/1.1"	
-	
-	db 11 
-	db 9
-	db "REMOTE_ADDR"
-	db "127.0.0.1"
-	
-	db 11
-	db 9
-	db "SERVER_NAME"
-	db "localhost"
+    ; Parameter 4: SERVER_PROTOCOL
+    db 0x0F, 0x00
+    db 0x08, 0x00
+    db 'SERVER_PROTOCOL', 0
+    db 'HTTP/1.1', 0
 
-	db 11
-	db 2
-	db "SERVER_PORT"
-	db "80"
+    ; Parameter 5: GATEWAY_INTERFACE
+    db 0x11, 0x00
+    db 0x07, 0x00
+    db 'GATEWAY_INTERFACE', 0
+    db 'CGI/1.1', 0
 
-	db 11
-	db 10
-	db "REQUEST_URI"
-	db "/index.php"
-
-fcgi_params_length = $ - fcgi_params ; Calculate the length of FCGI_PARAMS
+    ; Parameter 6: REQUEST_URI
+    db 0x0B, 0x00
+    db 0x0A, 0x00
+    db 'REQUEST_URI', 0
+    db '/index.php', 0
+fcgi_params_length = $ - fcgi_params
 
 fcgi_end_params:
     db 1,4,0,1, 0,0,0,0  ; type=4 (PARAMS), length=0
